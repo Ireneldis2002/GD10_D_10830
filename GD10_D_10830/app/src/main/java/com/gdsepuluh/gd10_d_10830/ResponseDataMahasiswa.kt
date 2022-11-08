44d4c8f291fff7c0d9377de7bf5c9ad93ef0cbd0
Ireneldis2002 <reneija6@gmail.com>
2022-11-08 09:46:00 +0700

    Upload GD10_D_10830 AndroidStudio

---
+++ GD10_D_10830/app/src/main/java/com/gdsepuluh/gd10_d_10830/ResponseDataMahasiswa.kt
@@ -0,0 +1,8 @@
+package com.gdsepuluh.gd10_d_10830
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseDataMahasiswa(
+    @SerializedName("status") val stt:String,
+    val totaldata: Int,
+    val data:List<MahasiswaData>)
